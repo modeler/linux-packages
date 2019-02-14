fd766e4200474a554e277724cca3a6a9e4f5ee0d
Dan Tootill <dantootill@gmail.com>
2019-02-14 14:55:19 +0000

    Fixed substitutions.

--- vice-2.4/build.sh
+++ vice-2.4/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 VERSION=2.4
-SOURCE=http://sourceforge.net/projects/vice-emu/files/releases/vice-${VERSION}/tar.gz/download
+SOURCE=http://sourceforge.net/projects/vice-emu/files/releases/vice-${VERSION}.tar.gz/download
 PACKAGE=vice
 
-wget ${SOURCE}/v${VERSION}.tar.gz
+wget --content-disposition ${SOURCE}
 
-mv v${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
+mv ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
 tar xf ${PACKAGE}_${VERSION}.orig.tar.gz
 
 cd ${PACKAGE}-${VERSION}
 dh_make -s -y
 cat ../control > debian/control
-cat ../rules >> debian/rules
+sed "s@CHANGEME@$(pwd)@g" ../rules >> debian/rules
 dpkg-buildpackage -b
 
 exit 0
