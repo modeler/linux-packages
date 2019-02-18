77a867b449d790a1e9db0d38b1715da841d3d522
Dan Tootill <dantootill@gmail.com>
2019-02-18 12:27:23 +0000

    Updated retroarch.

--- retroarch/build.sh
+++ retroarch/build.sh
@@ -4,6 +4,8 @@ VERSION=1.7.6
 SOURCE=https://github.com/libretro/RetroArch/archive
 PACKAGE=$(basename $(pwd))
 
+sudo apt install -y libavcodec-dev libavdevice-dev libavformat-dev libgbm-dev libglm-dev libjack-jackd2-dev libswscale-dev libusb-1.0-0-dev libv4l-dev libxml2-dev libvulkan-dev python3-dev qt5-default
+
 wget ${SOURCE}/v${VERSION}.tar.gz
 
 mv v${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
