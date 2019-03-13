d1efd6f38a73f05350d343088575d58efa637090
Dan Tootill <dantootill@gmail.com>
2019-03-13 10:47:09 +0000

    Experimental libusb1 build of OpenCBM.

---
+++ opencbm-libusb1/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=opencbm
+VERSION=0.4.99.99a
+URL=https://github.com/OpenCBM/OpenCBM/archive/libusb0_and_libusb1.zip
+ARCHIVE=OpenCBM-libusb0_and_libusb1.zip
+SOURCE=OpenCBM-libusb0_and_libusb1
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
