7b7bb617e515975d9459d9a236bc3d7c1fe2493d
Dan Tootill <dantootill@gmail.com>
2019-03-13 22:09:23 +0000

    Added alternate OpenCBM.

---
+++ opencbm-zyonee/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=opencbm
+VERSION=0.4.99.99a
+URL=https://github.com/zyonee/opencbm/archive/master.zip
+ARCHIVE=opencbm-master.zip
+SOURCE=opencbm-master
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
