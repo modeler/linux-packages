7096533da1c07a51a0964ee6f3541df27f40835c
Dan Tootill <dantootill@gmail.com>
2019-03-22 12:56:02 +0000

    New addition.

---
+++ ucon64/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=ucon64
+VERSION=2.1.0
+URL=https://downloads.sourceforge.net/ucon64/ucon64-${VERSION}-src.tar.gz
+ARCHIVE=
+SOURCE=ucon64-2.1.0-src
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
