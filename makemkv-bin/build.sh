edb1c2e6cc7357b12deb6fa62b018b8acd5fbab5
Daniel Tootill <dantootill@gmail.com>
2019-05-01 23:08:59 +0100

    New additions.

---
+++ makemkv-bin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=makemkv-bin
+VERSION=1.14.3
+URL=http://www.makemkv.com/download/makemkv-bin-${version}.tar.gz
+ARCHIVE=
+SOURCE=
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
---
+++ makemkv-oss/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=makemkv-oss
+VERSION=1.14.3
+URL=http://www.makemkv.com/download/makemkv-oss-${version}.tar.gz
+ARCHIVE=
+SOURCE=
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
