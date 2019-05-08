37db05292ddee38ad287214e18010de16b29274f
Dan Tootill <dantootill@gmail.com>
2019-05-08 13:31:01 +0100

    Retrieve beta key automatically.

--- makemkv-bin/build.sh
+++ makemkv-bin/build.sh
@@ -8,6 +8,8 @@ SOURCE=
 
 export PACKAGE VERSION URL ARCHIVE SOURCE
 
+curl https://www.makemkv.com/forum/viewtopic.php?t=1053 2> /dev/null | awk -F".code>" '/^The current beta key is/ { print $2 }' | tr -d '<' > beta_key.txt
+
 ../common.sh
 
 exit 0
