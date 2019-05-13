21f490c9f386adc29f929b1c1f17e7a14b9d8e91
Daniel Tootill <dantootill@gmail.com>
2019-05-13 11:14:29 +0100

    Update build.sh

--- retroarch-rpi/build.sh
+++ retroarch-rpi/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACKAGE=retroarch
-VERSION=1.7.6
+VERSION=1.7.7
 URL=https://github.com/libretro/RetroArch/archive/v${VERSION}.tar.gz
 ARCHIVE=RetroArch-${VERSION}.tar.gz
 SOURCE=RetroArch-${VERSION}
