82e5d944336333e77dc6ec9391f8bfc53448bc5c
Dan Tootill <dantootill@gmail.com>
2019-06-24 19:36:24 +0100

    Added 64tass.

---
+++ 64tass/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=64tass
+VERSION=1.54.1900
+URL=http://sourceforge.net/projects/tass64/files/source/64tass-${VERSION}-src.zip
+ARCHIVE=
+SOURCE=64tass-1.54.1900-src
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
