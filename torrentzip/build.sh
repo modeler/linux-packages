c68688d92231774181100863beee727e6bd85543
Dan Tootill <dantootill@gmail.com>
2019-08-23 23:49:10 +0100

    Added torrentzip, disable Wayland for RetroArch.

---
+++ torrentzip/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=torrentzip
+VERSION=svn
+URL=https://svn.code.sf.net/p/trrntzip/code
+ARCHIVE=
+SOURCE=code
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
