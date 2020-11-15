0ca22275c29b149672863fcb848a454ee270d5f5
Dan Tootill <dantootill@gmail.com>
2020-11-15 22:12:11 +0000

    Pi 4 build of RetroArch

---
+++ retroarch-rpi4/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=retroarch
+VERSION=1.8.9
+URL=https://github.com/libretro/RetroArch/archive/v${VERSION}.tar.gz
+ARCHIVE=RetroArch-${VERSION}.tar.gz
+SOURCE=RetroArch-${VERSION}
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
