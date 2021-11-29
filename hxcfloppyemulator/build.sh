46f19f134c07ea960db4e09649d26d3b7e1bffc8
Dan Tootill <dantootill@gmail.com>
2021-11-29 21:52:59 +0000

    Added HxC Floppy Drive Emulator.

---
+++ hxcfloppyemulator/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGE=hxcfloppyemulator
+VERSION=2.5.6.9
+URL=https://sourceforge.net/code-snapshots/svn/h/hx/hxcfloppyemu/code/hxcfloppyemu-code-r2509-HxCFloppyEmulator.zip
+ARCHIVE=
+SOURCE=hxcfloppyemu-code-r2509-HxCFloppyEmulator
+
+export PACKAGE VERSION URL ARCHIVE SOURCE
+
+../common.sh
+
+exit 0
