c1209fc5134d67d16e012444c1522d03750b0aa2
Daniel Tootill <dantootill@gmail.com>
2022-06-26 16:49:01 +0100

    Update build.sh

--- opencbm-git/build.sh
+++ opencbm-git/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PACKAGE=opencbm
-VERSION=0.4.99.99a
-URL=https://github.com/OpenCBM/OpenCBM/archive/master.zip
-ARCHIVE=OpenCBM-master.zip
-SOURCE=OpenCBM-master
+VERSION=0.4.99.104
+URL=https://github.com/OpenCBM/OpenCBM/archive/refs/tags/v${VERSION}.tar.gz
+ARCHIVE=
+SOURCE=OpenCBM-${VERSION}
 
 export PACKAGE VERSION URL ARCHIVE SOURCE
 
