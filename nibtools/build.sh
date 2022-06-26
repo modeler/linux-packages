5481c649a9a47c786e1f49ee5c49603410da88f1
Daniel Tootill <dantootill@gmail.com>
2022-06-26 18:56:38 +0100

    Update build.sh

--- nibtools/build.sh
+++ nibtools/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PACKAGE=nibtools
-VERSION=svn
-URL=https://c64preservation.com/svn/nibtools/trunk
+VERSION=194406f
+URL=https://github.com/OpenCBM/nibtools/archive/${VERSION}.zip
 ARCHIVE=
-SOURCE=
+SOURCE=nibtools-194406fd254e3a7069933d79e1657e828edc48ed
 
 export PACKAGE VERSION URL ARCHIVE SOURCE
 
